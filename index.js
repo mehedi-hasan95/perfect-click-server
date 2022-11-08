ade34d2c03bb4acaac160136ea7a045d5d728620
mehedititu <mh.me.mehedi@gmail.com>
2022-11-08 17:57:18 +0600

    Add data to mongodb

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // Midleware
 app.use(cors());
 app.use(express.json());
 
+
+// DB usrname = weddingPhotography
+// DB passwrod = rSiuCswBf35xqqOv
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const uri = "mongodb+srv://weddingPhotography:rSiuCswBf35xqqOv@cluster0.k4gmzpi.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try {
+        const weddingCollection = client.db("wedding").collection("details");
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = weddingCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch();
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
